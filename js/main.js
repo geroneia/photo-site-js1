015218af2417a570683eac196afa4c2f198c11eb
geroneia <kozetta2004@yandex.ru>
2020-01-30 20:25:56 +0300

    Вносит изменения в названия переменных

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict';
-var MIN_COMMENTS = 1;
-var MAX_COMMENTS = 5;
-var MIN_AVATAR_IMG = 1;
-var MAX_AVATAR_IMG = 6;
-var MIN_LIKES = 15;
-var MAX_LIKES = 200;
-var MIN_PHOTOS = 1;
+var MIN_COMMENTS_COUNT = 1;
+var MAX_COMMENTS_COUNT = 5;
+var MIN_AVATAR_IMG_COUNT = 1;
+var MAX_AVATAR_IMG_COUNT = 6;
+var MIN_LIKES_COUNT = 15;
+var MAX_LIKES_COUNT = 200;
+var MIN_PHOTOS_COUNT = 1;
 var PHOTOS_COUNT = 25;
 
 // Находит место, куда вставятся фотки
@@ -32,9 +32,9 @@ var getRandomInteger = function (min, max) {
 // Формирует массив из комментариев к фоткам
 var getComments = function () {
   var comments = [];
-  for (var i = 0; i < getRandomInteger(MIN_COMMENTS, MAX_COMMENTS); i++) {
+  for (var i = 0; i < getRandomInteger(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT); i++) {
     var comment = {};
-    comment['avatar'] = 'img/avatar-' + getRandomInteger(MIN_AVATAR_IMG, MAX_AVATAR_IMG) + '.svg';
+    comment['avatar'] = 'img/avatar-' + getRandomInteger(MIN_AVATAR_IMG_COUNT, MAX_AVATAR_IMG_COUNT) + '.svg';
     comment['message'] = messages[getRandomInteger(0, messages.length - 1)];
     comment['name'] = authorNames[getRandomInteger(0, authorNames.length - 1)];
     comments.push(comment);
@@ -44,7 +44,6 @@ var getComments = function () {
 
 // Собирает шаблон картинки с данными
 var getPicture = function (picture) {
-
   // клонирует шаблон
   var userPhoto = pictureTemplate.cloneNode(true);
 
@@ -58,9 +57,9 @@ var getPicture = function (picture) {
 var fragment = document.createDocumentFragment();
 for (var j = 0; j < PHOTOS_COUNT; j++) {
   var photo = {
-    url: 'photos/' + getRandomInteger(MIN_PHOTOS, PHOTOS_COUNT) + '.jpg',
+    url: 'photos/' + getRandomInteger(MIN_PHOTOS_COUNT, PHOTOS_COUNT) + '.jpg',
     description: '',
-    likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+    likes: getRandomInteger(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
     comments: getComments()
   };
   fragment.appendChild(getPicture(photo));

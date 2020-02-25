78c45ac68122b13ef8599ee113ac305fb68cc004
geroneia <kozetta2004@yandex.ru>
2020-02-26 00:58:17 +0300

    Добавляет работу фильтров популярных и случайных фотографий

---
+++ js/filter.js
@@ -0,0 +1,33 @@
+'use strict';
+(function () {
+  var filter = document.querySelector('.img-filters');
+  var buttons = filter.querySelectorAll('.img-filters__button');
+
+
+  window.filter = {
+    showButtons: function () {
+      filter.classList.remove('img-filters--inactive');
+    },
+    onButtonClick: function () {
+      filter.addEventListener('click', function (evt) {
+        // debugger
+        var target = evt.target;
+        var id = target.getAttribute('id');
+        if (target.classList.contains('img-filters__button')) {
+          for (var i = 0; i < buttons.length; i++) {
+            buttons[i].classList.remove('img-filters__button--active');
+          }
+          target.classList.add('img-filters__button--active');
+        }
+        if (id === 'filter-default') {
+          window.gallery.renderPhotos(window.gallery.getDefaultPhotos());
+        } else if (id === 'filter-random') {
+          window.gallery.renderPhotos(window.gallery.getUniqueRandomPhotos());
+        } else if (id === 'filter-discussed') {
+          window.gallery.renderPhotos(window.gallery.getDiscussedPhoto());
+        }
+      });
+    }
+  };
+
+})();
--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,7 @@
 'use strict';
 (function () {
   var PHOTOS_COUNT = 25;
+  var DISCUSSED_PHOTOS_COUNT = 10;
   var ENTER_KEY = 'Enter';
 
   var onEnterPress = function (evt) {
@@ -21,20 +22,71 @@
   // Находит место, куда вставятся фотки
   var anotherUserPictures = document.querySelector('.pictures');
   var images = [];
-  var onSuccessLoading = function (photos) {
-    var fragment = document.createDocumentFragment();
-    for (var j = 0; j < PHOTOS_COUNT; j++) {
-      var miniPhoto = {
-        url: photos[j].url,
-        description: '',
-        likes: photos[j].likes,
-        comments: window.data.getComments(photos[j].comments)
+  var photos = [];
+  var defaultPhotos = [];
+
+  window.gallery = {
+    renderPhotos: function (data) {
+      images = [];
+      var pictures = document.querySelectorAll('.picture');
+      for (var i = 0; i < pictures.length; i++) {
+        pictures[i].remove();
+      }
+      var fragment = document.createDocumentFragment();
+      fragment.innerHTML = '';
+      for (var j = 0; j < data.length; j++) {
+        var miniPhoto = {
+          url: data[j].url,
+          description: '',
+          likes: data[j].likes,
+          comments: window.data.getComments(data[j].comments)
+        };
+        fragment.appendChild(window.data.getPictureTemplate(miniPhoto));
+        images.push(miniPhoto);
+      }
+      // добавляет шаблон в отведенное место
+      anotherUserPictures.appendChild(fragment);
+    },
+    getDefaultPhotos: function () {
+      defaultPhotos = defaultPhotos.slice(0, PHOTOS_COUNT);
+      return defaultPhotos;
+    },
+    getUniqueRandomPhotos: function () {
+      var getRandomData = function (data) {
+        var j;
+        var temp;
+        for (var i = data.length - 1; i > 0; i--) {
+          j = Math.floor(Math.random() * (i + 1));
+          temp = data[j];
+          data[j] = data[i];
+          data[i] = temp;
+        }
+        return data;
       };
-      fragment.appendChild(window.data.getPictureTemplate(miniPhoto));
-      images.push(miniPhoto);
+
+      var mixedPhotos = getRandomData(photos);
+
+      var uniqueRandomPhotos = mixedPhotos.filter(function (it, i) {
+        return mixedPhotos.indexOf(it) === i;
+      });
+
+      uniqueRandomPhotos = uniqueRandomPhotos.slice(0, DISCUSSED_PHOTOS_COUNT);
+      return uniqueRandomPhotos;
+    },
+    getDiscussedPhoto: function () {
+      var discussedPhoto = photos.sort(function (a, b) {
+        return b.likes - a.likes;
+      });
+      discussedPhoto.slice(0, PHOTOS_COUNT);
+      return discussedPhoto;
     }
-    // добавляет шаблон в отведенное место
-    anotherUserPictures.appendChild(fragment);
+  };
+  var onSuccessLoading = function (data) {
+    defaultPhotos = data;
+    photos = data;
+    window.gallery.renderPhotos(window.gallery.getDefaultPhotos());
+    window.filter.showButtons();
+    window.filter.onButtonClick();
   };
 
   window.backend.load(onSuccessLoading);
